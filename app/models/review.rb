bb69dd1bb6aadb7f9a43ba76758275f380539c97
Monica Mow <mow.monica@gmail.com>
2016-06-21 16:56:57 -0700

    Generate review model
    - part 12 of tutorial
    - add review model
    - set up associations in Movie and User
    - set up validations (check if numericality syntax is correct)

---
+++ app/models/review.rb
@@ -0,0 +1,23 @@
+class Review < ActiveRecord::Base
+
+  belongs_to :user
+  belongs_to :movie
+
+  validates :user,
+    presence: true
+
+  validates :movie,
+    presence: true
+
+  validates :text
+    presence: true
+
+  validates :rating_out_of_ten,
+    presence: true,
+    numericality: { 
+      integer_only: true, 
+      greater_than_or_equal_to: 1,
+      less_than_or_equal_to: 10
+      }
+
+end
