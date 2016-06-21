251fdd7e1437ddbb3e8d3bbf94e072a4da45eceb
Monica Mow <mow.monica@gmail.com>
2016-06-21 11:19:40 -0700

    Add validations to Movie and display error messages in /new
    - parts 5 and 6 of tutorial

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,33 @@
 class Movie < ActiveRecord::Base
+  validates :title,
+    presence: true
+
+  validates :director,
+    presence: true
+
+  validates :description,
+    presence: true
+
+  validates :runtime_in_minutes,
+    presence: true,
+    numericality: { integer_only: true }
+
+  validates :poster_image_url,
+    presence: true
+
+  validates :release_date,
+    presence: true
+
+  validate :release_date_is_in_the_past
+
+  protected
+
+  # custom validation
+  def release_date_is_in_the_past
+    if release_date.present?
+      errors.add(:release_date, "should be in the past") if release_date > Date.today
+    end
+  end
+
 end
+
