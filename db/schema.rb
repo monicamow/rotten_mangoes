9388921c041fbca24a862f81af5ef7dd3582c481
Monica Mow <mow.monica@gmail.com>
2016-06-21 22:01:39 -0700

    Add boolean admin field to users table
    - users can be admin
      > by default, user is not admin
    - this way of dealing with admin-users should in theory be easy since a method can be used in the application controller to check if user is admin (similar to checking if current_user exists)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160621234241) do
+ActiveRecord::Schema.define(version: 20160622050038) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20160621234241) do
     t.datetime "release_date"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+    t.string   "image"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -39,10 +40,11 @@ ActiveRecord::Schema.define(version: 20160621234241) do
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "firstname"
     t.string   "lastname"
+    t.boolean  "admin",           default: false
   end
 
 end
