4d610ac93370a93929850305c94d00e4365513fb
Monica Mow <mow.monica@gmail.com>
2016-06-23 08:57:01 -0700

    Add a goodbye email for when user account deleted
    - edit the mailer (goodbye_email method)
    - create a goodbye mailer view
    - call the goodbye_email mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,4 +8,10 @@ class UserMailer < ApplicationMailer
     mail(to: @user.email, subject: 'Welcome to My Rotten Mangoes')
   end
 
+  def goodbye_email(user)
+    @user = user
+    @url  = 'http://example.com/signup'
+    mail(to: @user.email, subject: 'Goodbye :(')
+  end
+
 end
