288521c11a8a8ff29ca54ec06b5a7fa8b92d57a9
Monica Mow <mow.monica@gmail.com>
2016-06-23 19:02:59 -0700

    Fix image sizes on movie listing

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,7 +4,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  #include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -26,12 +26,17 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
-  #
+  
   # def scale(width, height)
   #   # do something
   # end
 
   # Create different versions of your uploaded files:
+
+  version :resized do
+    process :resize_to_limit => [500, 500]
+  end
+
   version :thumb do
     process :resize_to_limit => [200, 200]
   end
